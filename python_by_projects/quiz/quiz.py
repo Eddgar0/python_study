a0580abaff48a3f5ae498fe6947ba8407fe6c70b
Eddgar0 <erojas@u1965.edu.do>
2023-07-27 14:01:28 -0400

    Mejorando el formato de data

--- python_by_projects/quiz/quiz.py
+++ python_by_projects/quiz/quiz.py
@@ -12,9 +12,10 @@ NUM_PREGUNTAS_QUIZ = 5
 PREGUNTAS_PATH = pathlib.Path(__file__).parent / 'questions.toml'
 PREGUNTAS = tomllib.loads(PREGUNTAS_PATH.read_text())
 
-def prepara_preguntas(preguntas, num_preguntas):
+def prepara_preguntas(path, num_preguntas):
+    preguntas = tomllib.loads(path.read_text())['questions']
     num_preguntas = min(num_preguntas, len(preguntas))
-    return random.sample(list(preguntas.items()), k=num_preguntas)
+    return random.sample(list(preguntas), k=num_preguntas)
 
 
 def obten_respuesta(pregunta, opciones):
@@ -30,10 +31,11 @@ def obten_respuesta(pregunta, opciones):
     return opciones_etiquetadas[respuesta_etiqueta]
 
 
-def haz_pregunta(pregunta, opciones):
-    respuesta_correcta = opciones[0]
+def haz_pregunta(pregunta):
+    respuesta_correcta = pregunta['respuesta']
+    opciones = [pregunta['respuesta']] + pregunta['opciones']
     opciones_ordenadas = random.sample(opciones, k=len(opciones))
-    respuesta = obten_respuesta(pregunta, opciones_ordenadas)
+    respuesta = obten_respuesta(pregunta['pregunta'], opciones_ordenadas)
 
     if respuesta == respuesta_correcta:
         print('⭐ Correcto! ⭐')
@@ -44,12 +46,12 @@ def haz_pregunta(pregunta, opciones):
 
 
 def run_quiz():
-    preguntas = prepara_preguntas(PREGUNTAS, num_preguntas=NUM_PREGUNTAS_QUIZ)
+    preguntas = prepara_preguntas(PREGUNTAS_PATH, num_preguntas=NUM_PREGUNTAS_QUIZ)
     
     cantidad_correctas = 0 
-    for n, (pregunta, opciones) in enumerate(preguntas, start=1):
+    for n, pregunta in enumerate(preguntas, start=1):
          print(f'\nPregunta {n}:')
-         cantidad_correctas += haz_pregunta(pregunta, opciones)
+         cantidad_correctas += haz_pregunta(pregunta)
 
     print(f'\nObtuviste {cantidad_correctas} correctas de {n} preguntas')
 
