cac9ebb889e78c7133b14bb6965ec58b608c9a1c
Eddgar0 <guabeff@gmail.com>
2023-08-12 23:11:09 -0400

    Mail validation

--- HackerRank/validate_Mail.py
+++ HackerRank/validate_Mail.py
@@ -8,29 +8,17 @@
 # The website name can only have letters and digits .
 # The extension can only contain letters .
 # The maximum length of the extension is .
-
+import re
 
 
 def fun(s):
     # return True if s is a valid email, else return False
-    splitted_mail = s.plit('@')
+ matched = re.match(r'([A-Za-z0-9_\-])+@([A-Za-z0-9\.])+\.([A-Za-z]){2,3}$', s)
+ if matched:
+    return True
+ else:
+    return False
 
-    if len(splitted_mail) != 2:
-        return False
-    
-    #if 
-
-
-# verifico @ y parto por arroba si no hay exactamente 2, bobo error
-# Colocar el username aparte y analizarlo
-# colocar el dominio aparte y dividirlo de derecha a izquierda por punto 
-# Si tiene mas de 2 bobo error
-# validar 2 nivel de dominio
-# analizar el 1 nivel de dominio
-# si todos son verdaderos arroja verdadero
-
-
-    pass
 def filter_mail(emails):
     return list(filter(fun, emails))
 
