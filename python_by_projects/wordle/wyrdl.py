582ed6fda7ee115c3078ec0912a95be8429c3a19
Eddgar0 <erojas@u1965.edu.do>
2023-11-24 18:20:02 -0400

    adding modules and functions

--- python_by_projects/wordle/wyrdl.py
+++ python_by_projects/wordle/wyrdl.py
@@ -1,22 +1,54 @@
+# wyrdl.py
 import pathlib
 import random
+from string import ascii_letters
 
-WORDLIST = pathlib.Path('wordlist.txt')
-words = [word.upper() for word in WORDLIST.read_text('utf-8').strip().split('\n')]
-word = random.choice(words)
-# WORD = 'SNAKE'
-for guess_num in range(1,7):
-    guess = input(f'\nGuess {guess_num}: ').upper()
-    if guess == word:
-        print('Correct')
-        break
-    else:
-        print('Wrong')
+# ...
 
+def get_random_word():
+    wordlist = pathlib.Path(__file__).parent / 'wordlist.txt'
+    words = [
+        word.upper() for word in wordlist.read_text(encoding='utf-8').split('\n')
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+        ]
+    return random.choice(words)
+
+def show_guess(guess, word):
+    """Show the user's guess on the terminal and classify all letters.
+
+    ## Example:
+
+    >>> show_guess("CRANE", "SNAKE")
+    Correct letters: A, E
+    Misplaced letters: N
+    Wrong letters: C, R
+    """
     correct_letters = {letter for letter, correct in zip(guess, word) if correct == letter}
     misplaced_letters = set(guess) & set(word) - correct_letters
     wrong_letters = set(guess) - set(word)
     
-    print('Correct letters: ', ', '.join(sorted(correct_letters)))
-    print('Misplaced letters: ', ', '.join(sorted(misplaced_letters)))
-    print('Wrong letters: ', ', '.join(sorted(wrong_letters)))
+    print('Correct letters:', ', '.join(sorted(correct_letters)))
+    print('Misplaced letters:', ', '.join(sorted(misplaced_letters)))
+    print('Wrong letters:', ', '.join(sorted(wrong_letters)))
+
+def game_over(word):
+    print(f'The word was {word}' )    
+
+def main():
+    # Pre-process
+    word = get_random_word()
+    print(word)
+    # Process (main loop)
+    for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
+
+        show_guess(guess, word)
+        if guess == word:
+            break
+
+    # Post-process
+    else:
+        game_over(word)
+
+if __name__ == '__main__':
+    main()
